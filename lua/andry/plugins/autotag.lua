9ab07a476dfafecefefbe279ca5189e70ee944af
ATafa <atafa@ATafas-MacBook-Pro.local>
2026-02-13 16:55:16 +0000

    Changes to be committed: 	modified:   lua/andry/plugins/autotag.lua 	modified:   lua/andry/plugins/telescope.lua

--- lua/andry/plugins/autotag.lua
+++ lua/andry/plugins/autotag.lua
@@ -1,39 +1,41 @@
 local status, autotag = pcall(require, "nvim-ts-autotag")
 if not status then
-  return
+	return
 end
 
 autotag.setup({
-  enable_close = true,
-  enable_rename = true,
-  enable_close_on_slash = true,
-  per_filetype = {
-    ["html"] = {
-      enable_close = true
-    },
-    ["heex"] = {
-      enable_close = true
-    },
-    ["eelixir"] = {
-      enable_close = true
-    },
-    ["jsx"] = {
-      enable_close = true
-    },
-    ["tsx"] = {
-      enable_close = true
-    },
-    ["vue"] = {
-      enable_close = true
-    },
-    ["svelte"] = {
-      enable_close = true
-    },
-    ["php"] = {
-      enable_close = true
-    },
-    ["xml"] = {
-      enable_close = true
-    },
-  }
+	opts = {
+		enable_close = true,
+		enable_rename = true,
+		enable_close_on_slash = true,
+	},
+	per_filetype = {
+		["html"] = {
+			enable_close = true,
+		},
+		["heex"] = {
+			enable_close = true,
+		},
+		["eelixir"] = {
+			enable_close = true,
+		},
+		["jsx"] = {
+			enable_close = true,
+		},
+		["tsx"] = {
+			enable_close = true,
+		},
+		["vue"] = {
+			enable_close = true,
+		},
+		["svelte"] = {
+			enable_close = true,
+		},
+		["php"] = {
+			enable_close = true,
+		},
+		["xml"] = {
+			enable_close = true,
+		},
+	},
 })
